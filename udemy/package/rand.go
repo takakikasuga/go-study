b4f63af1dfdd0b059042260cbe6388abf0d95d37
takakikasuga <takaki.soccer1@gmail.com>
2023-04-26 11:34:08 +0900

    📦️ rand

---
+++ udemy/package/rand.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// @desc rand
+func main() {
+	rand.Seed(256)
+
+	fmt.Println(rand.Float64())
+	fmt.Println(rand.Float64())
+	fmt.Println(rand.Float64())
+
+	// 現在の時刻をシードに使った擬似乱数の生成
+	fmt.Println(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
+	// 0~99の疑似乱数
+	fmt.Println(rand.Intn(100))
+	// int型の疑似乱数
+	fmt.Println(rand.Int())
+	// int32型の疑似乱数
+	fmt.Println(rand.Int31())
+	// int64型の疑似乱数
+	fmt.Println(rand.Int63())
+	// unit32型の疑似乱数
+	fmt.Println(rand.Uint32())
+}
