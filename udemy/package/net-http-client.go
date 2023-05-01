0a4ecef2ffc22546f3081e90d72b3406dc9bb4be
takakikasuga <takaki.soccer1@gmail.com>
2023-05-01 09:27:41 +0900

    📦️ net/http/client

---
+++ udemy/package/net-http-client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+// @desc net/http/client
+func main() {
+	// GETメソッド
+	/*
+		res, _ := http.Get("https://example.com/")
+
+		fmt.Println(res.StatusCode)
+		fmt.Println(res.Proto)
+		fmt.Println(res.Header["Date"])
+		fmt.Println(res.Header["Content-Type"])
+		fmt.Println(res.Header)
+		fmt.Println(res.Request.Method)
+		fmt.Println(res.Request.URL)
+
+		defer res.Body.Close()
+
+		body, _ := ioutil.ReadAll(res.Body)
+
+		fmt.Println(string(body))
+	*/
+
+	// POSTメソッド
+	vs := url.Values{}
+
+	vs.Add("id", "1")
+	vs.Add("message", "メッセージ")
+	fmt.Println(vs.Encode())
+	res, err := http.PostForm("https://example.com/", vs)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+}
